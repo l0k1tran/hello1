e85d1bc50b1314ca6e4548e08ecef50d141b8ba6
l0k1tran <95475039+l0k1tran@users.noreply.github.com>
2023-05-31 14:54:15 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String ditmemaytien = "thay doi tu tien sang lui ";
+        String a = "khi nao co tien thi dung di can gio nua nha";
     }
 }
