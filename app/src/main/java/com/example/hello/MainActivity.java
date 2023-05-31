7e75591c3c28fbacc89bdd8f4ff41bcc69bddaad
Kuroky <optimusss247@gmail.com>
2023-05-31 14:47:59 +0700

    tat ca la tai tien

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String ditmemaytien = "thay doi tu tien sang lui ";
     }
 }
